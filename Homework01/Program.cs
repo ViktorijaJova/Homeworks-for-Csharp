72184fc8a6d0417f2258ec36b97879cea5b99695
ViktorijaJova <60360925+ViktorijaJova@users.noreply.github.com>
2020-03-03 16:54:35 +0100

    Add files via upload

---
+++ Homework01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace Homework01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Traffic light...");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine(" -----------------");
+            Console.WriteLine(" |Red light STOP!|" );
+            Thread.Sleep(2000);
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(" |Yellow PREPARE!|");
+            Thread.Sleep(2000);
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine(" |Green light GO!|");
+            Thread.Sleep(2000);
+            Console.WriteLine(" -----------------");
+
+            Console.ReadLine();
+        }
+    }
+}
