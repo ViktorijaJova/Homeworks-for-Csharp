3f94c1ba1c053df630a0efd2a5cb9e980e572ec7
ViktorijaJova <60360925+ViktorijaJova@users.noreply.github.com>
2020-03-26 16:48:56 +0100

    Add files via upload

---
+++ Homework05/Dog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework05.classes
+{
+    public class Dog
+    {
+        public string  DogName { get; set; }
+        public string DogColor { get; set; }
+        public string DogRace { get; set; }
+       
+        public string Eat()
+        {
+            return $"{DogName}  which is a {DogColor} {DogRace} is now eating";
+        }
+        
+        public string Play()
+        {
+            return $"{DogName} which is  a {DogColor} {DogRace} is now Playing";
+        }
+        public string ChasingTail()
+        {
+            return $"{DogName} which is a { DogColor} {DogRace} is now chasing it's tail";
+        }
+    }
+}
---
+++ Homework05/Driver.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+using System.Text;
+
+namespace Homework05.classes
+{
+ public class Driver
+    {
+        public string Name { get; set; }
+        public byte Skill { get; set; }
+        public bool IsSelected { get; set; }
+
+        public Driver()
+        {
+            Name = "unknown";
+            IsSelected = false;
+        }
+
+        public Driver(string name, byte skill)
+        {
+            Name = name;
+            Skill = skill;
+            IsSelected = false;
+        }
+    }
+
+    public class Car
+    {
+        public string Model { get; set; }
+        public int Speed { get; set; }
+        public Driver Driver { get; set; }
+        public bool IsSelected { get; set; }
+
+        public int CalculateSpeed(Driver driverObject)
+        {
+            int result = driverObject.Skill * Speed;
+            return result;
+        }
+
+        public Car()
+        {
+            IsSelected = false;
+        }
+
+        public Car(string model, int speed)
+        {
+            Model = model;
+            Speed = speed;
+            Driver = new Driver();
+            IsSelected = false;
+        }
+    }
+}
