5f6c2b932ea9f8f20392ce3c571b47d3fa9df37c
ViktorijaJova <60360925+ViktorijaJova@users.noreply.github.com>
2020-04-04 21:03:14 +0200

    Add files via upload

---
+++ Homework06.2/Models/User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework06.App.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public long CardNumber { get; set; }
+        private string Pin { get; set; }
+        private double Balance;
+
+        public User(string username, long cardNumber, string pin, double balance)
+        {
+            CardNumber = cardNumber;
+            Username = username;
+            Pin = pin;
+            Balance = balance;
+        }
+
+        public void CashWithdraw(double sum)
+        {
+            Balance -= sum;
+        }
+        public void CashDeposit(double sum)
+        {
+            Balance += sum;
+        }
+        public double GetBalance()
+        {
+            return Balance;
+        }
+
+        public string GetPin()
+        {
+            return Pin;
+        }
+
+    }
+
+}
