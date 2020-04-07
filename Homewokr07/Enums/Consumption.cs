e50e61747c2b7af3f436571270be774806a871fc
ViktorijaJova <60360925+ViktorijaJova@users.noreply.github.com>
2020-04-07 21:36:03 +0200

    Add files via upload

---
+++ Homewokr07/Enums/Consumption.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homewokr07.Enums
+{
+   public  enum Consumption
+    {
+        economic =1,
+        medium = 2,
+        high = 3
+    }
+}
