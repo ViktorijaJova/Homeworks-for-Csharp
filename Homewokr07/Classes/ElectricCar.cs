3fa3eaff0995733545d807ca93aa57b22752042c
ViktorijaJova <60360925+ViktorijaJova@users.noreply.github.com>
2020-04-08 13:48:00 +0200

    Add files via upload

---
+++ Homewokr07/Classes/ElectricCar.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using Homewokr07.Enums;
+
+namespace Homewokr07.Enteties
+{
+    public class ElectricCar : Car
+    {
+        public int BateryCapacity { get; set; }
+        public int BateryUsage { get; set; }
+        public ElectricCar(string brand, string model, int doors, int topSpeed,Consumption consumption,EngineType engineType,int batteryCapacity, int batteryUsage) : base(brand, model, doors, topSpeed,consumption,engineType)
+        {
+            BateryCapacity = batteryCapacity;
+            BateryUsage = batteryUsage;
+        }
+
+        public void Drive(int distance)
+
+
+        {
+            var usedBattery = (distance * Convert.ToInt32(Consumption) / 10);
+            BateryUsage = BateryUsage - (distance * Convert.ToInt32(Consumption) / 10);
+            if (usedBattery > BateryCapacity)
+            {
+                Console.WriteLine($"You can't drive more than the capacity of your battery which is {BateryCapacity} percents");
+            }
+            else
+            {
+                Console.WriteLine($"For the distance you Drove you used {usedBattery} percent of the battery and now you have {BateryUsage}percents left ");
+            }
+        }
+
+        public void Recharge( int minutes)
+        {
+            var rechargablePercents = minutes / 10;
+            BateryUsage = BateryUsage + rechargablePercents;
+            if ( rechargablePercents > BateryCapacity)
+            {
+                Console.WriteLine($"Can't Recharge for {BateryUsage} percent  because it bigger than the BatteryCapacity which is {BateryCapacity}percents");
+            }
+            else
+            {
+                
+                Console.WriteLine($"Your battery has been Recharged for:{rechargablePercents} percents");
+            }
+
+
+
+
+        }
+    }
+}
---
+++ Homewokr07/Classes/FuelCar.cs
@@ -0,0 +1,54 @@
+﻿using Homewokr07.Enums;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+
+namespace Homewokr07.Enteties
+{
+   public class FuelCar : Car {
+       
+        public int FuelCapacity { get; set; }
+        public int CurentFuel { get; set; }
+    public FuelCar(string brand, string model, int doors, int topSpeed,Consumption consumption,EngineType engineType, int fuelCapacity, int currentFuel)
+            : base(brand, model, doors, topSpeed,consumption,engineType)
+        {
+            FuelCapacity = fuelCapacity;
+            CurentFuel = currentFuel;
+        }
+
+
+        public void Drive(int distance)
+        {
+
+            var usedFuel = (distance * Convert.ToInt32(Consumption) / 10);
+            CurentFuel = CurentFuel - usedFuel;
+
+            if (usedFuel > FuelCapacity)
+            {
+                Console.WriteLine($"You cant drive more than your fuel capacity which is {FuelCapacity}L");
+            }
+            else
+            {
+                Console.WriteLine($"For the distance you Drove you used {usedFuel}L Fuel and have left {CurentFuel}L");
+            }
+        }
+
+        public void Refule(int fuel)
+        {
+            if (fuel > FuelCapacity)
+            {
+                Console.WriteLine($"You can't refule {fuel}L cause your above the limit of your fuel capacity which is {FuelCapacity}L");
+            }
+            else 
+            {
+             var result = (CurentFuel += fuel);
+             Console.WriteLine($"Your car has been Refueld for:{fuel}L");
+
+            }
+
+
+
+        }
+    }
+}
