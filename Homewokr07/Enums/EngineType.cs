6fd3bdbaab6bf300ee9c2c80de69f1a6cb4d6141
ViktorijaJova <60360925+ViktorijaJova@users.noreply.github.com>
2020-04-08 13:52:38 +0200

    Add files via upload

---
+++ Homewokr07/Enums/EngineType.cs
@@ -0,0 +1,20 @@
+﻿using Homewokr07.Enteties;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homewokr07.Enums
+{
+    public enum EngineType
+    {
+        
+
+         Petrol =  1,
+        Disel = 2,
+        Electric = 3,
+
+       
+            
+    
+}
+}
