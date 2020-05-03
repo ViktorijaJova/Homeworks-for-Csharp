9ff2a75744b8837c0db86d0c844c9ae6f20c69fe
ViktorijaJova <60360925+ViktorijaJova@users.noreply.github.com>
2020-05-03 21:33:09 +0200

    Add files via upload

---
+++ Homework08/Enteties/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hoemwork08e.Entites
+{
+   public enum Category
+    {
+        ItEquipment = 0,
+        LapTop = 1,
+        PC = 2,
+        TV = 3,
+    }
+}
