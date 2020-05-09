41fed8306f9544b6090ed531cf1754c2350cd1bb
ViktorijaJova <60360925+ViktorijaJova@users.noreply.github.com>
2020-05-09 16:43:55 +0200

    Add files via upload

---
+++ Homework10/Services/UserInterface.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TryHomework
+{
+   public class UserInterface : Validate
+    {
+
+        public void ChoseCinema()
+        {
+            Console.WriteLine("Please chose which cinema you want to go to: 1 or 2 ");
+            Console.WriteLine("1.Pathe");
+            Console.WriteLine("2. Cineplex");
+        }
+
+        public void ChoseCinemaOptions()
+        {
+            Console.WriteLine("Please choose from the following options: 1 or 2 ");
+            Console.WriteLine("1. See all of the movie lists");
+            Console.WriteLine("2. See movies by Genre");
+        }
+
+        public void Movies(List<Movie> allMovies)
+        {
+            foreach (var movie in allMovies)
+            {
+                Console.WriteLine($"{movie.Title}");
+            }
+        }
+
+        public void MovieGenre()
+        {
+            Console.WriteLine("Please choose your favourite Genre: 1 to 5");
+            Console.WriteLine("1. Comedy");
+            Console.WriteLine("2. Horror");
+            Console.WriteLine("3. Action");
+            Console.WriteLine("4. Drama");
+            Console.WriteLine("5. SciFi");
+        }
+
+
+
+
+    }
+}
---
+++ Homework10/Services/Validate.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TryHomework
+{
+     public class Validate
+    {
+        public string ValidateOption1or2()
+        {
+            string input = Console.ReadLine();
+
+
+            try
+            {
+
+                if ((input != "1") && (input != "2"))
+
+                {
+                    throw new Exception("Please chose between option 1 or 2, thank you");
+
+
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                input = ValidateOption1or2();
+
+            }
+
+            return input;
+
+
+        }
+
+        public string ValidateOption1to5()
+        {
+            string input = Console.ReadLine();
+
+            try
+            {
+                if ((input != "1") && (input != "2") && (input != "3") && (input != "4") && (input != "5"))
+                {
+                   throw new Exception("Please chose between options 1 to 5, thank you");
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                input = ValidateOption1to5();
+
+            }
+
+            return input;
+        }
+
+        public Movie Movie(List<Movie> allMovies)
+        {
+            string input = Console.ReadLine();
+            Movie movieToWatch = null;
+            try
+            {
+
+                foreach (var movie in allMovies)
+                {
+                    if (input == movie.Title)
+                    {
+                        movieToWatch = movie;
+                    }
+                }
+                if (movieToWatch == null)
+                {
+                    throw new Exception("Please choose a  movie to watch from the list");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                movieToWatch = Movie(allMovies);
+
+            }
+
+            return movieToWatch;
+        }
+
+        public Genre Genres(string genre)
+        {
+            Genre chosenGenre = Genre.Random;
+
+            switch (genre)
+            {
+                case "1":
+                    chosenGenre = Genre.Comedy;
+                    break;
+                case "2":
+                    chosenGenre = Genre.Horror;
+                    break;
+                case "3":
+                    chosenGenre = Genre.Action;
+                    break;
+                case "4":
+                    chosenGenre = Genre.Drama;
+                    break;
+                case "5":
+                    chosenGenre = Genre.SciFi;
+                    break;
+            }
+            return chosenGenre;
+        }
+    }
+}
