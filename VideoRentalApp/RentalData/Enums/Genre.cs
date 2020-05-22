cb3d0c4e78776aed6e049cf992b9847af69d137f
ViktorijaJova <60360925+ViktorijaJova@users.noreply.github.com>
2020-05-22 15:58:08 +0200

    Add files via upload

---
+++ VideoRentalApp/RentalData/Enums/Genre.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sedc.videorental.data.Enums
+{
+   public enum Genre
+    {
+        Comedy = 0,
+        Horror = 1,
+        SciFi = 2,
+        Drama = 3,
+        Action = 4
+    }
+}
