a11dad786fc707371e1be6674cb014031ae00126
ViktorijaJova <60360925+ViktorijaJova@users.noreply.github.com>
2020-05-24 21:24:57 +0200

    Add files via upload

---
+++ VideoRentalApp/RentalData/Enums/Discount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Encodings.Web;
+
+namespace sedc.videorental.data.Enums
+{
+   public enum Discount
+    {
+        TenPercent = 1,
+        FiftyPercent = 2,
+        AlmostFree = 3,
+        None = 4
+   
+    }
+}
