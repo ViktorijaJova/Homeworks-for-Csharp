82bed4e040aeaefb3221db9674cd234099abe69f
ViktorijaJova <60360925+ViktorijaJova@users.noreply.github.com>
2020-06-05 21:35:36 +0200

    Add files via upload

---
+++ QuizApp/Data/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace quiz.data.Models
+{
+   public class User
+    {
+
+        public string Name { get; set; }
+        public int Password { get; set; }
+
+        public bool isTeacher { get; set; }
+
+        public bool TestDone { get; set; }
+
+/*        public List<int>TestsDone { get; set; }
+*/
+    }
+}
