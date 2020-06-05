986529b8c05238dc8091aab04d7b47e50b1fe61d
ViktorijaJova <60360925+ViktorijaJova@users.noreply.github.com>
2020-06-05 21:44:14 +0200

    Add files via upload

---
+++ QuizApp/Services/Helpers/Helpers.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace quiz.Services.Services
+{
+    public class Helpers
+    {
+       
+
+            private static readonly List<string> _validConfirmInput = new List<string> { "Y", "y", "yes", "Yes", "1", "True" };
+            private static readonly List<string> _validDeclineInput = new List<string> { "n", "N", "No", "no", "0", "False" };
+            public static int ToInteger(int min, int max)
+            {
+                int parsedNumber = 0;
+                bool isValid = false;
+
+                while (!isValid)
+                {
+                    try
+                    {
+                        parsedNumber = int.Parse(Console.ReadLine());
+                        if (!(parsedNumber >= min && parsedNumber <= max))
+                        {
+                            throw new Exception($"Please select from given input range from {min} to {max}");
+                        }
+                        isValid = !isValid;
+                    }
+                    catch (ArgumentNullException)
+                    {
+                        Console.WriteLine("Please enter argument");
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Not valid input");
+                    }
+                    catch (OverflowException)
+                    {
+                        Console.WriteLine("Number is too large or too low");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                return parsedNumber;
+            }
+
+        
+        public static bool ToConfirm()
+        {
+            bool isValidInput;
+            while (true)
+            {
+                var value = Console.ReadLine();
+                if (_validConfirmInput.Contains(value))
+                {
+                    isValidInput = true;
+                    break;
+                }
+                else if (_validDeclineInput.Contains(value))
+                {
+                    isValidInput = false;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter valid input");
+                    Console.WriteLine($"Valid inputs:{string.Join(",", _validConfirmInput.ToArray())}{string.Join(",", _validDeclineInput.ToArray())}");
+                }
+
+            }
+            return isValidInput;
+        }
+    }
+
+
+}
